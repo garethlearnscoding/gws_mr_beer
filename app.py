c3a1d8b3928d3e7b1bf02da167e28ab1ee80a28c
KRUSTstation <seayadakshat@gmail.com>
2025-05-23 10:57:16 +0800

    Made Basic Layout

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def base():
     return render_template('base.html')
 
+@app.route('/messages')
+def displayMessages():
+    return render_template('./messagesTemplate/messages.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
